b76204e7e5e3789427734d6f4065a4479474e603
coopcuk2 <crcooper@outlook.com>
2017-07-11 14:43:32 +0100

    Camera On test

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -2,12 +2,18 @@ package com.develogical.camera;
 
 public class Camera {
 
+    private Sensor sensor;
+
+    public Camera(Sensor _sensor) {
+        this.sensor = _sensor;
+    }
+
     public void pressShutter() {
         // not implemented
     }
 
     public void powerOn() {
-        // not implemented
+        this.sensor.powerUp();
     }
 
     public void powerOff() {
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -6,8 +6,17 @@ import org.mockito.ArgumentCaptor;
 import static org.mockito.Mockito.*;
 
 public class CameraTest {
+
+    Sensor mockSensor = mock(Sensor.class);
+
+
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
-        // write your test here
+
+        Camera c = new Camera(mockSensor);
+         c.powerOn();
+
+         verify(mockSensor).powerUp();
+
     }
 }
